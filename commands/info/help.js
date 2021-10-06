d845d0a163455896120f2219ed710fa04d90b261
DanericNetwork <danericaalbers@hotmail.com>
2021-10-06 22:42:21 +0200

    made help command

---
+++ commands/info/help.js
@@ -0,0 +1,22 @@
+const { Message, MessageEmbed, Client } = require("discord.js");
+const config = require('../../config.json');
+
+module.exports = {
+    name: "help",
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        const pr = config.prefix;
+        const embed = new MessageEmbed()
+        .setColor('#328732')
+        .setTitle('Information')
+        .addField(`Info`, `\`${pr}businfo\`, \`${pr}busses\`, \`${pr}stats\``)
+        .addField(`Admins`, `\`${pr}addbus\`, \`${pr}update\``)
+        .setFooter('Bus Bot', client.user.avatarURL());
+
+        message.channel.send({content: null, embeds: [embed] });
+    },
+};
