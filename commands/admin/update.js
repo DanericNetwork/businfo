9e2b65e0fae4908915ad9f785a108d627a8fc29c
DanericNetwork <danericaalbers@hotmail.com>
2021-10-06 20:55:24 +0200

    Updated bus update command

--- commands/admin/update.js
+++ commands/admin/update.js
@@ -13,10 +13,19 @@ module.exports = {
             {
               id: 'all',
             });
+            
+// Arguments
+        const busnumber = parseInt(args[0]);
+        const curbs = parseInt(args[1]);
+        const delay = parseInt(args[2]);
+        const skips = parseInt(args[3]);
+        
+        var statscountbus = await statsmodel.findOne(
+          {
+            id: busnumber,
+          });
 
-        const curbs = parseInt(args[0]);
-        const delay = parseInt(args[1]);
-        const skips = parseInt(args[2]);
+// Checks
 
         if(isNaN(curbs)) return message.channel.send("Please enter a valid number of curbs.");
         if(isNaN(delay)) return message.channel.send("Please enter a valid delay.");
@@ -25,6 +34,10 @@ module.exports = {
         const newcurb = statscount.curb + curbs;
         const newdelay = statscount.delay + delay;
         const newskips = statscount.skips + skips;
+
+        const newbuscurb = statscountbus.curb + curbs;
+        const newbusdelay = statscountbus.delay + delay;
+        const newbusskips = statscountbus.skips + skips;
         await statsmodel.findOneAndUpdate(
             {
                 id: 'all',
@@ -38,13 +51,29 @@ module.exports = {
               upsert: true,
             }
           )
+        await statsmodel.findOneAndUpdate(
+            {
+                id: busnumber,
+            },
+            {
+              curb: newbuscurb,
+              delay: newbusdelay,
+              skips: newbusskips,
+            },
+            {
+              upsert: true,
+            }
+          )
           statscount.save();
         const embed = new MessageEmbed()
         .setColor('#328732')
-        .setTitle('Stats Updated')
+        .setTitle(`Stats Updated for ${busnumber}`)
         .addField('New Total Curbs Hit', `${newcurb}`, true)
         .addField("New Total Delay", `${newdelay} minutes`, true)
         .addField("New Total Skips", `${newskips}`, true)
+        .addField(`New ${busnumber} Curbs Hit`, `${newbuscurb}`, true)
+        .addField(`New ${busnumber} Delay`, `${newbusdelay} minutes`, true)
+        .addField(`New ${busnumber} Skips`, `${newbusskips}`, true)
         message.channel.send({content: null, embeds: [embed] });
     },
 };
