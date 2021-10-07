3607983aa5f3f02a44a139a91547228a6074bdc0
DanericNetwork <danericaalbers@hotmail.com>
2021-10-07 11:39:10 +0200

    Added credits for a specific bus

--- commands/info/businfo.js
+++ commands/info/businfo.js
@@ -10,6 +10,7 @@ module.exports = {
      */
     run: async (client, message, args) => {
         const busnumber = args[0];
+        const yanick = client.users.cache.get('165339399922515968');
         var statscount = await statsmodel.findOne(
             {
               id: busnumber,
@@ -17,10 +18,14 @@ module.exports = {
         const embed = new MessageEmbed()
         .setColor('#328732')
         .setTitle(`Stats for bus ${busnumber}`)
-        .setDescription(`These are some stats that we've collected from the RRReis busses!\n*These stats are collected from 10/5/2021 and are from the busses that we've been in!*`)
-        .addField('Total Curbs Hit', `${statscount.curb}`, true)
-        .addField("Total Delay", `${statscount.delay} minutes`, true)
-        .addField("Total Stops skipped", `${statscount.skips}`, true)
+        if(args[0] == "5737"){
+            embed.setDescription(`These stats of bus 5737 are collected by ${yanick.tag}.`)
+        } else {
+        embed.setDescription(`These are some stats that we've collected from the RRReis busses!\n*These stats are collected from 10/5/2021 and are from the busses that we've been in!*`)
+        }
+        embed.addField('Total Curbs Hit', `${statscount.curb}`, true)
+        embed.addField("Total Delay", `${statscount.delay} minutes`, true)
+        embed.addField("Total Stops skipped", `${statscount.skips}`, true)
         message.channel.send({content: null, embeds: [embed] });
     },
 };
