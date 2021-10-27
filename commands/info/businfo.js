44e905cfd932f96c88b325da89b9d93ac038975e
DanericNetwork <danericaalbers@hotmail.com>
2021-10-27 22:14:11 +0200

    Cleaned up some messy code

--- commands/info/businfo.js
+++ commands/info/businfo.js
@@ -13,17 +13,20 @@ module.exports = {
         const yanick = client.users.cache.get('165339399922515968');
         const daneric = client.users.cache.get('244060616686305283');
         const frank = client.users.cache.get('207557174660956160');
+
         var statscount = await statsmodel.findOne(
             {
               id: busnumber,
             });
+        
         const embed = new MessageEmbed()
         .setColor('#328732')
         .setTitle(`Stats for bus ${busnumber}`)
+        
         if(args[0] == "5737"){
             embed.setDescription(`These stats of bus 5737 are collected by ${yanick.tag}.`)
         } else {
-        embed.setDescription(`These are some stats that we've collected from the RRReis busses by ${daneric.tag} & ${frank.tag}!\n*These stats are collected from 10/5/2021 and are from the busses that we've been in!*`)
+            embed.setDescription(`These are some stats that we've collected from the RRReis busses by ${daneric.tag} & ${frank.tag}!\n*These stats are collected from 10/5/2021 and are from the busses that we've been in!*`)
         }
         embed.addField('Total Curbs Hit', `${statscount.curb}`, true)
         embed.addField("Total Delay", `${statscount.delay} minutes`, true)
