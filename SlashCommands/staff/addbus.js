83faab772b990417d5e45a97ae80e9fdc44e747b
Daneric <danericaalbers@hotmail.com>
2021-11-04 17:55:15 +0100

    Made addbus command in slashcommands

--- SlashCommands/staff/addbus.js
+++ SlashCommands/staff/addbus.js
@@ -1,35 +1,26 @@
 const { Client, CommandInteraction } = require("discord.js");
 const { MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders")
 const config = require('../../config.json');
 const statsmodel = require('../../models/stats');
 
 module.exports = {
-    name: "addbus2",
-    description: "Get stats about the RRReis busses that we've been in.",
-    options: [
-        {
-          type: 3,
-          name: "busnumber",
-          description: "busnummer",
-          required: true
-        },
-        {
-          type: 3,
-          name: "BusTrain",
-          description: "Bus or Train",
-          choices: [
-            {
-              name: "bus",
-              value: "bus"
-            },
-            {
-              name: "train",
-              value: "train"
-            }
-          ],
-          required: true
-        }
-      ],
+    ...new SlashCommandBuilder()
+        .setName("addbus")
+        .setDescription("Get stats about the RRReis busses that we've been in.")
+
+        .addIntegerOption(option =>
+          option.setName('busnum')
+            .setDescription('The number of the bus or train')
+            .setRequired(true))
+            
+        .addStringOption(option =>
+          option.setName('ovtype')
+            .setDescription('Bus or Train')
+            .setRequired(true)
+            .addChoice('Bus', 'bus')
+            .addChoice('Train', 'train')),
+
     /**
      *
      * @param {Client} client
@@ -38,14 +29,14 @@ module.exports = {
      */
     run: async (client, interaction, args) => {
     // Checks if author is an admin
-    if(!config.admins.includes(message.author.id)) return message.channel.send("You do not have permission to use this command!");
+    if(!config.admins.includes(interaction.member.id)) returninteraction.followUp("You do not have permission to use this command!");
 
-    const busnumber = parseInt(args[0]);
-    const ovtype = args[1];
+    const busnumber = interaction.options.getInteger('busnum');
+    const ovtype = interaction.options.getString('ovtype');
 
-    if(isNaN(busnumber)) return message.channel.send("Please enter a valid busnumber."); // Checks if busnumber is a number
-    if(!ovtype) return message.channel.send("Please enter a valid ovtype."); // Checks if ovtype is given
-    if(!["train", "bus"].includes(ovtype)) return message.channel.send(`Please enter a valid type. Usage: \`${config.prefix}addbus <bus/train number> bus/train\``); // Checks if ovtype is a bus or train
+    if(isNaN(busnumber)) return interaction.followUp("Please enter a valid busnumber."); // Checks if busnumber is a number
+    if(!ovtype) return interaction.followUp("Please enter a valid ovtype."); // Checks if ovtype is given
+    if(!["train", "bus"].includes(ovtype)) return interaction.followUp(`Please enter a valid type. Usage: \`${config.prefix}addbus <bus/train number> bus/train\``); // Checks if ovtype is a bus or train
     let statscount = await statsmodel.create(
           { 
             id: busnumber, 
@@ -56,16 +47,9 @@ module.exports = {
           });
     statscount.save();
 
-        const user = client.users.cache.get(interaction.member.id);
-
-        const embed = new MessageEmbed()
-            .setTitle(`Current bus stats!`)
-            .setColor("#328732")
-            .setDescription(`These are some stats that we've collected from the RRReis busses!\n*These stats are collected from 10/5/2021 and are from the busses that we've been in!*`)
-            .addField("Total Curbs Hit", `${statscount.curb}`, true)
-            .addField("Total Delay", `${statscount.delay} minutes`, true)
-            .addField("Total Stops skipped", `${statscount.skips}`, true)
-            .setFooter(`${user.username}`, user.avatarURL())
+    const embed = new MessageEmbed()
+        .setColor('#328732')
+        .setTitle(`Bus/Train ${busnumber} added!`)
 
         interaction.followUp({content: null, embeds: [embed] });
             
