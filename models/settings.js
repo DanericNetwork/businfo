d351692dd7b0b683c87e92e487ef56f7ddd44b89
Daneric <danericaalbers@hotmail.com>
2022-03-02 22:38:26 +0100

    feat: settings model total

--- models/settings.js
+++ models/settings.js
@@ -2,5 +2,6 @@ const mongoose = require("mongoose");
 const settingsSchema = new mongoose.Schema({
     id: { type: String, default: 'data' },
     admins: { type: Array },
+    totalritten: { type: Number },
 });
 module.exports = mongoose.model("settings", settingsSchema);
